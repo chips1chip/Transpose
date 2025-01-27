53438d6bdce5ca1e5bec4084b094c29644c7b84c
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-27 17:00:40 +0530

    Update Transpose.java

--- Transpose.java
+++ Transpose.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter the no.of rows: ");
-        int rows = sc.nextInt();
+        int r = sc.nextInt();
         System.out.println("enter the no.of columns: ");
-        int columns = sc.nextInt();
-        int[][] mat= new int[rows][columns];
-        int[][] transpose =new int[rows][columns];
+        int c = sc.nextInt();
+        int[][] mat= new int[r][c];
+        int[][] transpose =new int[r][c];
         System.out.println("enter elements  of matrix: ");
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 mat[i][j] =  sc.nextInt();
             }
         }
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 transpose[j][i]= mat[i][j];
             }
             System.out.println();
         }
         System.out.println("transpose matrix: ");
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 System.out.print(transpose[i][j]+" ");
             }
             System.out.println();
